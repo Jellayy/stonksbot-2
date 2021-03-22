f790ea26a7d15efdeecf37dd6608354b0d55285c
Jellayy <austinlynnhuffman@gmail.com>
2021-03-21 19:30:51 -0700

    Accounts for an error when querying outside of market hours

--- discord_integration.py
+++ discord_integration.py
@@ -34,16 +34,19 @@ async def stonk(ctx, stock=None, timespan='day', multiplier=int(1), start='2021-
     else:
         r = requests.get(f'https://api.polygon.io/v2/aggs/ticker/{stock.upper()}/range/{multiplier}/{timespan}/{start}/{end}?apiKey={VERY_SECRET_LAUNCH_CODES.HYDROGEN_LAUNCH_CODE()}')
 
-        # Copy pasted code to format dataframe for mplfinance
-        df = pd.DataFrame(r.json()['results'])
-        est = pytz.timezone('US/Eastern')
-        utc = pytz.utc
-        df.index = [datetime.utcfromtimestamp(ts / 1000.).replace(tzinfo=utc).astimezone(est) for ts in df['t']]
-        df.index.name = 'Date'
-        df.columns = ['Volume', 'Volume Weighted', 'Open', 'Close', 'High', 'Low', 'Time', 'Num Items']
+        try:
+            # Copy pasted code to format dataframe for mplfinance
+            df = pd.DataFrame(r.json()['results'])
+            est = pytz.timezone('US/Eastern')
+            utc = pytz.utc
+            df.index = [datetime.utcfromtimestamp(ts / 1000.).replace(tzinfo=utc).astimezone(est) for ts in df['t']]
+            df.index.name = 'Date'
+            df.columns = ['Volume', 'Volume Weighted', 'Open', 'Close', 'High', 'Low', 'Time', 'Num Items']
 
-        mpf.plot(df, type='candle', volume=True, style='mike', savefig='plot.png')
-        file = discord.File('plot.png', filename="plot.png")
-        await ctx.channel.send(file=file)
+            mpf.plot(df, type='candle', volume=True, style='mike', savefig='plot.png')
+            file = discord.File('plot.png', filename="plot.png")
+            await ctx.channel.send(file=file)
+        except KeyError:
+            await ctx.channel.send(embed=await embeds.stonk_syntax_error(client))
 
 client.run(VERY_SECRET_LAUNCH_CODES.PLUTONIUM_LAUNCH_CODE())
--- embeds.py
+++ embeds.py
@@ -5,8 +5,9 @@ async def stonk_syntax_error(client):
     embed = discord.Embed(
         color=discord.Color.red(),
         title="Invalid Syntax",
-        description="**Usage:** gib stonk [TICKER] [(opt)START(YYYY-M-D)] [(opt)END(YYYY-M-D)]"
+        description="**Usage:** gib stonk [TICKER] (TIMEFRAME) (MULTIPLIER) (START: YYYY-MM-DD) (END: YYYY-MM-DD)"
     )
     embed.set_footer(text="StonksBotDos", icon_url=client.user.avatar_url)
+    embed.add_field(name="\u200B", value="*querying outside of market hours can throw this exception as well*")
 
     return embed
