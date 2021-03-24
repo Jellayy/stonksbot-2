67be782f16cf1f911279141fd2c7a5a7f73e5b4d
Jellayy <austinlynnhuffman@gmail.com>
2021-03-23 22:11:18 -0700

    did stuff

--- api.py
+++ api.py
@@ -1,39 +1,26 @@
-from polygon import RESTClient
+import requests
 import pandas as pd
-from datetime import datetime
-import datetime as dt
 import pytz
-import asyncio
+from datetime import datetime
+import mplfinance as mpf
 import VERY_SECRET_LAUNCH_CODES
 
 
-async def get_polygon_dataframe(ticker, fromdate, todate):
-    key = VERY_SECRET_LAUNCH_CODES.HYDROGEN_LAUNCH_CODE()
-    with RESTClient(key) as client:
-        resp = client.stocks_equities_aggregates(ticker=ticker, from_=fromdate, to=todate, multiplier="5",
-                                                 timespan="minute")
-        data = resp.results
-        # print(data)
-        if data:
-            for i in data:
-                # print(i["t"], i["vw"])
-                date = datetime.fromtimestamp(i["t"] / 1000, tz=pytz.timezone('US/Eastern')).strftime("%Y-%m-%d "
-                                                                                                      "%H:%M:%S")
-                i["t"] = date
-                # print(i)
-            df = pd.DataFrame.from_dict(data).rename(
-                columns={"v": "Volume", "vw": "Average", "o": "Open", "c": "Close", "h": "High", "l": "Low",
-                         "t": "Time"}).set_index("Time")
-            if 'n' in df.columns:
-                df = df.drop(columns=["n"])
-                return df
-            else:
-                # print(df)
-                return df
-        else:
-            print(data)
-            print("Dictionary is empty")
-            return False
+def gen_graph(stock, timespan, multiplier, start, end):
+    r = requests.get(f'https://api.polygon.io/v2/aggs/ticker/{stock.upper()}/range/{multiplier}/{timespan}/{start}/{end}?apiKey={VERY_SECRET_LAUNCH_CODES.HYDROGEN_LAUNCH_CODE()}')
 
+    # Copy pasted code to format dataframe for mplfinance
+    df = pd.DataFrame(r.json()['results'])
+    est = pytz.timezone('US/Eastern')
+    utc = pytz.utc
+    df.index = [datetime.utcfromtimestamp(ts / 1000.).replace(tzinfo=utc).astimezone(est) for ts in df['t']]
+    df.index.name = 'Date'
+    df.columns = ['Volume', 'Volume Weighted', 'Open', 'Close', 'High', 'Low', 'Time', 'Num Items']
 
-# print(asyncio.run(get_polygon_dataframe("GME", "2021-03-07", "2021-03-14")))
+    # Very dank custom mpl theme copyright your mom LLC
+    mc = mpf.make_marketcolors(up='#00a320', down='#ff3334', inherit=True)
+    s = mpf.make_mpf_style(base_mpf_style='nightclouds', marketcolors=mc, gridaxis='horizontal', gridcolor='#454545', gridstyle='solid', edgecolor='#454545', facecolor='#000000')
+
+    # Saves graph
+    saving_params = dict(fname='plot.png', dpi=1200)
+    mpf.plot(df, type='candle', volume=True, style=s, savefig=saving_params, vlines=dict(vlines=['2021-03-18 09:30', '2021-03-18 16:00'], linewidths=0.2, colors="#454545"))
--- discord_integration.py
+++ discord_integration.py
@@ -1,12 +1,11 @@
 import discord
 from discord.ext import commands
-import mplfinance as mpf
-import pandas as pd
-import pytz
-from datetime import datetime
-import requests
+import datetime as dt
 import embeds
+import api
 import VERY_SECRET_LAUNCH_CODES
+
+five_day_delta = dt.timedelta(days=5)
 
 client = commands.Bot(command_prefix='gib ')
 
@@ -28,27 +27,12 @@ Latency: {round(client.latency*1000, 3)}ms
 
 # This was made in like 2 hours for testing but it works ez clap
 @client.command()
-async def stonk(ctx, stock=None, timespan='day', multiplier=int(1), start='2021-01-01', end=datetime.today().strftime("%Y-%m-%d")):
+async def stonk(ctx, stock=None, timespan='minute', multiplier=int(5), start=((dt.datetime.today()-five_day_delta).strftime("%Y-%m-%d")), end=dt.datetime.today().strftime("%Y-%m-%d")):
     if stock is None:
         await ctx.channel.send(embed=await embeds.stonk_syntax_error(client))
     else:
-        r = requests.get(f'https://api.polygon.io/v2/aggs/ticker/{stock.upper()}/range/{multiplier}/{timespan}/{start}/{end}?apiKey={VERY_SECRET_LAUNCH_CODES.HYDROGEN_LAUNCH_CODE()}')
-
         try:
-            # Copy pasted code to format dataframe for mplfinance
-            df = pd.DataFrame(r.json()['results'])
-            est = pytz.timezone('US/Eastern')
-            utc = pytz.utc
-            df.index = [datetime.utcfromtimestamp(ts / 1000.).replace(tzinfo=utc).astimezone(est) for ts in df['t']]
-            df.index.name = 'Date'
-            df.columns = ['Volume', 'Volume Weighted', 'Open', 'Close', 'High', 'Low', 'Time', 'Num Items']
-
-            # Very dank custom mpl theme copyright your mom LLC
-            mc = mpf.make_marketcolors(up='#00a320', down='#ff3334', inherit=True)
-            s = mpf.make_mpf_style(base_mpf_style='nightclouds', marketcolors=mc, gridaxis='horizontal', gridcolor='#454545', gridstyle='solid', edgecolor='#454545', facecolor='#000000')
-
-            saving_params = dict(fname='plot.png', dpi=1200)
-            mpf.plot(df, type='candle', volume=True, style=s, savefig=saving_params)
+            api.gen_graph(stock, timespan, multiplier, start, end)
             file = discord.File('plot.png', filename="plot.png")
             await ctx.channel.send(file=file, embed=await embeds.stonk_view(client, stock.upper()))
         except KeyError:
