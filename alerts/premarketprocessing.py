b2ea4cd912fa844dbed791fc3ac582124185259a
Benjamin Huth <benjaminmhuth@gmail.com>
2021-08-17 20:28:47 -0700

    Premarket processing start

---
+++ alerts/premarketprocessing.py
@@ -0,0 +1,59 @@
+import asyncio
+import csv
+
+import aiohttp
+import time
+
+from configparser import ConfigParser
+
+from pandas import read_csv
+
+parser = ConfigParser()
+parser.read('../config.ini')
+MONITORED_STOCKS = parser.get('General', 'monitored stocks').split(",")
+api_parser = ConfigParser()
+api_parser.read('../api.ini')
+POLYGON_KEY = api_parser.get('APIs', 'polygon api key')
+
+with open('stockslist.csv', newline='') as f:
+    reader = csv.reader(f)
+    stock_list = list(reader)
+
+yeet = []
+
+
+async def get(list):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                    url="https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/tickers/{"
+                        "}?&apiKey=xGSFtFGNqZMzAq64YNMNEdfk7mGY4KNT".format(
+                        list[0])) as response:
+                gays = await response.json()
+
+                if gays['ticker']['day']['vw'] < 20:
+                    print(list[0])
+                    yeet.append(list[0])
+
+    except Exception as e:
+        print("Unable to get url {} due to {}.".format(list, e.__class__))
+
+
+async def main(list):
+    # print(list)
+    all_responses = await asyncio.gather(*[get(url) for url in list])
+    # print(all_responses)
+    print("Finalized all. ret is a list of len {} outputs.".format(len(all_responses)))
+
+
+# urls = websites.split("\n")
+# print(ticker_list)
+num_urls = len(stock_list)
+
+start = time.time()
+asyncio.get_event_loop().run_until_complete(main(stock_list))
+end = time.time()
+
+print("Took {} seconds to pull {} websites.".format(end - start, num_urls))
+print(yeet)
+print(len(yeet))
